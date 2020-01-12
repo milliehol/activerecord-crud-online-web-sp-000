75d2710b147cfb0df4cdcc30721dfe23d6ab34bf
Carla Holcomb <carla.programmer@gmail.com>
2020-01-12 17:18:02 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,12 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie.title = attributes[:title]
+  movie.release_date = attributes[:release_date]
+  movie.director = attributes[:director]
+  movie.lead = attributes[:lead] 
+  movie.in_theaters = attributes[:in_theaters]
+  movie.save
 end
 
 def can_be_created_in_a_block(args = __)

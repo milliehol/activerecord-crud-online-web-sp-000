9a4762fcca06e2f61c1c326a402849f5228e96e0
Carla Holcomb <carla.programmer@gmail.com>
2020-01-12 17:18:43 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,6 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
+  movie=movie.new
   movie.title = attributes[:title]
   movie.release_date = attributes[:release_date]
   movie.director = attributes[:director]
